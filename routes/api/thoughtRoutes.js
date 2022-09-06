402b298b31aeaef1a67556fee8d21c07b56f48d9
LisaTLe <lisa.le1127@gmail.com>
2022-09-05 18:49:38 -0600

    thought route edit

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,1 +1,32 @@
 const router = require("express").Router();
+
+const {
+  getAllThoughts,
+  getThoughtsById,
+  createThoughts,
+  updateThoughts,
+  deleteThoughts,
+  addReaction,
+  deleteRaction,
+} = require("../../controllers/thought-controller");
+
+//GET requests for /api/thoughts
+router.route("/").get(getAllThoughts);
+
+//GET, PUT, and DELETE requests for /api/thoughts/:id
+router
+  .route("/:id")
+  .get(getThoughtsById)
+  .put(updateThoughts)
+  .delete(deleteThoughts);
+
+//POST requests for /api/thoughts/:userId
+router.route("/:userId").post(createThoughts);
+
+//POST request for /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
+
+//DELETE request for /api/thoughts/:thoughtId/reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteRaction);
+
+module.exports = router;
