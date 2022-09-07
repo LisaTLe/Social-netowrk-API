05ac2b44b2b56a94836543e4ab55d4f3222f2dd3
LisaTLe <lisa.le1127@gmail.com>
2022-09-06 20:03:15 -0600

    fix typos

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 app.use(require("./routes"));
 
@@ -18,7 +18,7 @@ app.use(require("./routes"));
 // });
 
 mongoose.connect(
-  process.env.MONODB_URI || "mongodb://localhost/social-network-api",
+  process.env.MONGODB_URI || "mongodb://localhost/social-network-api",
   {
     useFindAndModify: false,
     useNewUrlParser: true,
